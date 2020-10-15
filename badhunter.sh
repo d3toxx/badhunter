b8094ba975790c2004a0f7769305e4b9179a316e
d3toxx <42433252+d3toxx@users.noreply.github.com>
2020-10-14 22:37:29 -0700

    Add files via upload

---
+++ badhunter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Author: Steven aka d3toxxx
+############################################
+############################################
+
+#The purpose of this script is facilitate the ability 
+#for blue teamers to pull and IR package from a Linux host.
+
+( cat /etc/passwd;
+  egrep ':0+' /etc/passwd
+  cat /etc/shadow/;
+  cat /etc/group;
+  cat /etc/sudoers;
+  cat /root/.ssh/authorized_keys;
+  cat /root/.bash_history;
+
+  netstat -antup;
+  netstat -plantux;
+  netstat -nap;
+
+  ps -aux;
+
+  cat /etc/crontab;
+) > results.txt
